969b2a0931af325a3087a219fcd1bea416a07254
caiconghao <caiconghao@flyfinger.com>
2019-05-20 17:29:58 +0800

    v.0.00

---
+++ js/canvas.js
@@ -0,0 +1,12 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+c.beginPath();
+
+c.moveTo(0, window.innerHeight / 2);
+c.lineTo(window.innerWidth , window.innerHeight / 2);
+
+c.stroke();
